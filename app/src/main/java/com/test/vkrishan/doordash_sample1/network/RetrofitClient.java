7e430575af575be01f4b6fc564455eb1142f74da
Vaibhav Krishan <vkrishan@vkrishan-macmini.roam.corp.google.com>
2018-08-22 20:21:22 -0700

    Added layout

--- app/src/main/java/com/test/vkrishan/doordash_sample1/network/RetrofitClient.java
+++ app/src/main/java/com/test/vkrishan/doordash_sample1/network/RetrofitClient.java
@@ -8,9 +8,12 @@ public class RetrofitClient {
     private static final String BASE_URL = "https://api.doordash.com";
     private static Retrofit retrofit;
 
-    
+    public static Retrofit getRetrofitInstance(){
 
-
-
+        if(retrofit == null){
+            retrofit  = new retrofit2.Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+        return retrofit;
+    }
 
 }
